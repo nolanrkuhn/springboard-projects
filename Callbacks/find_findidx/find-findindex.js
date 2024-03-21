53b0b4d7c1d0fcb09ef750ff2c49151117001863
nolanrkuhn <nolanrkuhn@gmail.com>
2024-03-20 17:34:21 -0700

    find and findIndex assignment

---
+++ Callbacks/find_findidx/find-findindex.js
@@ -0,0 +1,20 @@
+const usersArray = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function(user) {
+    return user.username === username
+  })
+}
+
+function removeUser(usersArray, username) {
+  let foundIndex = usersArray.findIndex(function(user) {
+    return user.username === username
+  })
+  if(foundIndex === -1) return;
+
+  return usersArray.splice(foundIndex,1)[0]
+}
