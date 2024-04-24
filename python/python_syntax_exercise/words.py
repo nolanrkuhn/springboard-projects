cfd57aafb3d7884ad091431c3ea1e26d5f860a33
nolanrkuhn <nolanrkuhn@gmail.com>
2024-04-23 17:01:01 -0700

    First python exercise

---
+++ python/python_syntax_exercise/words.py
@@ -0,0 +1,18 @@
+def print_upper_words(words, must_start_with):
+    """Print each word on sep line, uppercased, if starts with one of given
+
+        >>> print_upper_words3(["eagle", "Edward", "Alfred", "zope"],
+        ...                   must_start_with=["A", "E"])
+        EDWARD
+        ALFRED
+    """
+
+    for word in words:
+        for letter in must_start_with:
+            if word.startswith(letter):
+                print(word.upper())
+                break
+
+
+print_upper_words(["hello", "hey", "goodbye", "yo", "yes"], "h")
+				  
